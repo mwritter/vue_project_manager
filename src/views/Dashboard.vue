d3e0593673596ea45780575937a3f5338849ea03
Matthew Ritter <mritter2283@gmail.com>
2019-02-09 16:16:36 -0500

    Added template data for what each project entry is going to look like on the dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,7 +2,26 @@
   <div class="dashboard">
     <h1 class="subheading grey--text">Dashboard</h1>
     <v-container class="my-5">
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores non architecto eligendi magnam quisquam nesciunt omnis fuga, possimus pariatur ad officia ducimus! Recusandae quae quibusdam consequatur voluptatem nemo dolor fuga!</p>
+      <v-card flat class="pa-3">
+        <v-layout row wrap>
+          <v-flex xs12 md4>
+            <div class="caption grey--text">Project Title</div>
+            <div>Create a new website</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Person</div>
+            <div>Matthew</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Due by</div>
+            <div>3ed Mar 2019</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Status</div>
+            <div>ongoing</div>
+          </v-flex>
+        </v-layout>
+      </v-card>
     </v-container>
   </div>
 </template>

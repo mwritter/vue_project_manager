5eae70a5c2011579e052701cd6979e44d13edcf6
Matthew Ritter <mritter2283@gmail.com>
2019-02-09 12:26:01 -0500

    Added links to drawer to dashboard, my projects, and team

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,13 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
-import 'vuetify/src/stylus/app.styl'
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
+import "vuetify/src/stylus/app.styl";
 
 Vue.use(Vuetify, {
-  iconfont: 'md',
-})
+  iconfont: "md",
+  theme: {
+    primary: "#9652ff",
+    success: "#3cd1c2",
+    info: "#ffaa2c",
+    error: "#f83e70"
+  }
+});

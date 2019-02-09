81683a9e53e47af43376ae19f6a4e8a91df11dbb
Matthew Ritter <mritter2283@gmail.com>
2019-02-09 12:11:41 -0500

    Added drawer to project

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,7 @@
 <template>
   <nav>
     <v-toolbar flat app>
+      <v-toolbar-side-icon class="grey--text" @click="drawer =!drawer"></v-toolbar-side-icon>
       <v-toolbar-title class="text-uppercase grey--text">
         <span class="font-weight-light">Todo</span>
         <span>Manager</span>
@@ -11,9 +12,19 @@
         <v-icon right>exit_to_app</v-icon>
       </v-btn>
     </v-toolbar>
+
+    <v-navigation-drawer v-model="drawer" app class="blue">
+      <p></p>
+    </v-navigation-drawer>
   </nav>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      drawer: false
+    };
+  }
+};
 </script>

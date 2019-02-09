79aec8e3a37b3cd8fe5b90ab5db6de2d1796ccc8
Matthew Ritter <mritter2283@gmail.com>
2019-02-09 12:06:30 -0500

    Added styled toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
-  <v-app>
+  <v-app class="grey lighten-4">
     <v-content>
+      <Navbar/>
       <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
+import Navbar from "@/components/Navbar";
+
 export default {
   name: "App",
+  components: { Navbar },
   data() {
     return {
       //

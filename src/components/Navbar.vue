b3cf518bf17dd0d61b57b7cc094a948868f1553f
Matthew Ritter <mritter2283@gmail.com>
2019-02-11 20:58:28 -0500

    Addednew component to add projects from drawer

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -35,6 +35,9 @@
           </v-avatar>
           <p class="white--text subheading mt-1">Matthew Ritter</p>
         </v-flex>
+        <v-flex class="mt-4 mb-4">
+          <Popup/>
+        </v-flex>
       </v-layout>
       <v-list>
         <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
@@ -51,7 +54,11 @@
 </template>
 
 <script>
+import Popup from "./Popup";
 export default {
+  components: {
+    Popup
+  },
   data() {
     return {
       drawer: false,

b840bdd73168faa956d205d7e4c3e7b2a64ff9e1
Matthew Ritter <mritter2283@gmail.com>
2019-02-11 21:15:47 -0500

    Added all input fields for adding a new project, including a datepicker component from vuetify

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -5,11 +5,47 @@
       <v-card-title>
         <h2>Add a New Project</h2>
       </v-card-title>
+      <v-card-text>
+        <v-form class="px-3">
+          <v-text-field label="Title" v-model="title" prepend-icon="folder"></v-text-field>
+          <v-textarea label="Information" v-model="content" prepend-icon="edit"></v-textarea>
+          <v-menu>
+            <v-text-field
+              :value="formattedDate"
+              label="Due date"
+              slot="activator"
+              prepend-icon="date_range"
+            ></v-text-field>
+            <v-date-picker v-model="due"></v-date-picker>
+          </v-menu>
+          <v-spacer></v-spacer>
+          <v-btn flat class="success mx-0 mt-3" @click="submit">Add project</v-btn>
+        </v-form>
+      </v-card-text>
     </v-card>
   </v-dialog>
 </template>
 <script>
-export default {};
+import format from "date-fns/format";
+export default {
+  data() {
+    return {
+      title: "",
+      content: "",
+      due: null
+    };
+  },
+  methods: {
+    submit() {
+      console.log(this.title, this.content);
+    }
+  },
+  computed: {
+    formattedDate() {
+      return this.due ? format(this.due, "Do MMM YYYY") : "";
+    }
+  }
+};
 </script>
 <style>
 </style>
